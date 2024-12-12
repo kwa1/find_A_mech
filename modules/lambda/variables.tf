d72ac440e4ea3c1898b92ccb49789f168c4ee7d9
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-12 21:26:29 +0000

    Update variables.tf

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,1 +1,28 @@
+# modules/lambda/variables.tf
+
+variable "function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "handler" {
+  description = "The handler for the Lambda function"
+  type        = string
+}
+
+variable "runtime" {
+  description = "The runtime for the Lambda function"
+  type        = string
+}
+
+variable "source_path" {
+  description = "The source path for the Lambda code"
+  type        = string
+}
+
+variable "lambda_role_name" {
+  description = "The name of the IAM role for the Lambda function"
+  type        = string
+  default     = "lambda-role"
+}
 
