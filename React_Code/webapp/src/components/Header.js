753688d6104404311fa9cfd73a44b0fa3717da70
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-12 23:02:47 +0000

    Create Header.js

---
+++ React_Code/webapp/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Find a Mechanic</h1>
+      <p>Enter your location to find the nearest mechanics.</p>
+    </header>
+  );
+}
+
+export default Header;
