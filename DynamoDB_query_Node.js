4b07c4bfedc0d40220a4440d48bbe209326fe26b
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-12 20:39:35 +0000

    Create DynamoDB_query_Node.js

---
+++ DynamoDB_query_Node.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk');
+const location = new AWS.Location();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    const { latitude, longitude } = JSON.parse(event.body);
+    
+    // Call AWS Location Service to calculate nearest mechanics
+    const response = await location.searchPlaceIndexForPosition({
+        IndexName: "MechanicLocationIndex",
+        Position: [longitude, latitude]
+    }).promise();
+
+    const mechanics = response.Results.map(result => ({
+        name: result.Place.Label,
+        phone: result.Place.PhoneNumber,
+        distance: result.Place.Distance
+    }));
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ mechanics })
+    };
+};
